20a95f3a01a454690675e5f12ba6231bddd733d7
hfpp2012copy <hfpp2012@gmail.com>
2024-03-26 15:10:15 +0800

    fix

--- src/models/task.ts
+++ src/models/task.ts
@@ -6,7 +6,8 @@ export interface ITask extends Document {
   uploadedFile: string; // 上传文件路径或URL
   user: Schema.Types.ObjectId; // 对User模型的引用
   orderTime: Date; // 下单时间
-  orderType: 'NormalOrder' | 'SpecificTimeOrder'; // 下单类型
+  orderTimeType: 'NormalOrder' | 'SpecificTimeOrder'; // 下单时间类型
+  orderType: 'Normal' | 'ContactForVolumeWeight' | 'ContactForInventory' | 'ContactForPrice';
   orderNote?: string; // 下单备注，可选字段
   reviewType?: 'NormalReview' | 'ReviewAfterModification'; // 评价类型
   reviewFile?: string; // 评价文件路径或URL，用于评价后补
@@ -15,12 +16,13 @@ export interface ITask extends Document {
 const TaskSchema: Schema = new Schema({
   country: { type: String, required: true },
   file: { type: String, required: true },
-  uploadedFile: { type: String, required: true },
+  uploadedFile: { type: String, required: false },
   user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  orderTime: { type: Date, required: true },
-  orderType: { type: String, required: true, default: 'NormalOrder' },
+  orderType: { type: String, required: true, enum: ['Normal', 'ContactForVolumeWeight', 'ContactForInventory', 'ContactForPrice'], default: 'Normal' }, // 下单时间类型
+  orderTime: { type: Date, required: false },
+  orderTimeType: { type: String, required: true, enum: ['NormalOrder', 'SpecificTimeOrder'], default: 'NormalOrder' }, // 下单时间类型
   orderNote: { type: String, required: false, default: '' }, // 下单备注，默认为空字符串
-  reviewType: { type: String, required: false, default: 'NormalReview' }, // 评价类型，默认为'NormalReview'
+  reviewType: { type: String, required: true, enum: ['NormalReview', 'ReviewAfterModification'], default: 'NormalReview' }, // 评价类型，默认为'NormalReview'
   reviewFile: { type: String, required: false }, // 评价文件路径或URL，可选字段
 }, { timestamps: true });
 
