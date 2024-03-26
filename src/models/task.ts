8f89a2152438e70637a2aa3aaff53d5ddc732d5f
hfpp2012copy <hfpp2012@gmail.com>
2024-03-26 14:24:46 +0800

    fix

--- src/models/task.ts
+++ src/models/task.ts
@@ -6,8 +6,10 @@ export interface ITask extends Document {
   uploadedFile: string; // 上传文件路径或URL
   user: Schema.Types.ObjectId; // 对User模型的引用
   orderTime: Date; // 下单时间
+  orderType: 'NormalOrder' | 'SpecificTimeOrder'; // 下单类型
   orderNote?: string; // 下单备注，可选字段
-  review?: string; // 评价，可选字段
+  reviewType?: 'NormalReview' | 'ReviewAfterModification'; // 评价类型
+  reviewFile?: string; // 评价文件路径或URL，用于评价后补
 }
 
 const TaskSchema: Schema = new Schema({
@@ -16,8 +18,10 @@ const TaskSchema: Schema = new Schema({
   uploadedFile: { type: String, required: true },
   user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   orderTime: { type: Date, required: true },
-  orderNote: { type: String, required: false }, // 下单备注
-  review: { type: String, required: false }, // 评价
+  orderType: { type: String, required: true, default: 'NormalOrder' },
+  orderNote: { type: String, required: false, default: '' }, // 下单备注，默认为空字符串
+  reviewType: { type: String, required: false, default: 'NormalReview' }, // 评价类型，默认为'NormalReview'
+  reviewFile: { type: String, required: false }, // 评价文件路径或URL，可选字段
 }, { timestamps: true });
 
 export default mongoose.model<ITask>('Task', TaskSchema);
