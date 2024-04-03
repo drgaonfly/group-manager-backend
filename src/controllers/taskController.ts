be104a2a38ace19af91e4c73b9c6ff3cfe239a72
hfpp2012copy <hfpp2012@gmail.com>
2024-04-03 15:10:46 +0800

    fix tasks

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -31,7 +31,7 @@ export const createTask = handleAsync(async (req: RequestCustom, res: Response) 
 
 export const getAllTasks = handleAsync(async (req: Request, res: Response) => {
   // 从请求的查询参数中获取country, platform和status
-  const { country, platform, status } = req.query;
+  const { country, platform, status, _id, orderTimeType, reviewType } = req.query;
 
   // 构建一个查询对象，仅当提供了相应的查询参数时才添加对应的过滤条件
   const queryConditions: any = {};
@@ -44,6 +44,15 @@ export const getAllTasks = handleAsync(async (req: Request, res: Response) => {
   if (status) {
     queryConditions.status = status;
   }
+  if (_id) {
+    queryConditions._id = _id;
+  }
+  if (orderTimeType) {
+    queryConditions.orderTimeType = orderTimeType;
+  }
+  if (reviewType) {
+    queryConditions.reviewType = reviewType;
+  }
 
   // 使用过滤条件执行查询，并填充user字段以获取用户详情
   const tasks = await Task.find(queryConditions).populate('user');
@@ -51,9 +60,10 @@ export const getAllTasks = handleAsync(async (req: Request, res: Response) => {
   // 使用定义的函数来处理 file 字段
   const modifiedFileTasks = await transformDocumentImages(tasks, ['file', 'uploadedFile']);
   
-  // 返回查询结果
+  // 返回查询结果，指定要返回的字段
   res.status(200).json({ success: true, data: modifiedFileTasks });
 });
+
 
 
 export const getTaskById = handleAsync(async (req: Request, res: Response) => {
