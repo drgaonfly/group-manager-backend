9b3e16367c0f5ba89370efd513ef5687878263f6
dragonfly <2237676323@qq.com>
2025-01-03 13:54:47 +0800

    add instructions

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,7 @@ import topicRoutes from './routes/topicRoutes';
 import answerRoutes from './routes/answerRoutes';
 import proxyRoutes from './routes/proxyRoutes';
 import employeeRoutes from './routes/employeeRoutes';
+import instructionRoutes from './routes/instructionRoutes';
 
 import withdrawRoutes from './routes/withdrawRoutes';
 import instructionRoutes from './routes/instructionRoutes';
@@ -63,6 +64,7 @@ app.use('/api/answers', answerRoutes);
 app.use('/api/topics', topicRoutes);
 app.use('/api/proxies', proxyRoutes);
 app.use('/api/employees', employeeRoutes);
+app.use('/api/instructions', instructionRoutes);
 
 app.use('/api/withdraws', withdrawRoutes);
 
