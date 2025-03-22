3c09ce7f14120f51eb65c3f33513cee24734fae8
efihfaifjw <2218344129@qq.com>
2025-03-22 15:05:43 +0800

    fix: update

--- src/routes/withdrawRoutes.ts
+++ src/routes/withdrawRoutes.ts
@@ -16,11 +16,14 @@ import {
 
 const router: Router = express.Router();
 
+// 添加提现记录
+router.post('/withdraw', customerProtect, addWithdraw);
+
 // 设置提现记录的路由
 router
   .route('/')
   .get(protect, checkPermission, getWithdraws)
-  .post(customerProtect, addWithdraw)
+  .post(protect, checkPermission, addWithdraw)
   .delete(protect, checkPermission, deleteMultipleWithdraws);
 
 router
