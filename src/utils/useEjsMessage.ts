b8d799acfe53072892fce03156db94e7acc7d1cc
dragonfly <2237676323@qq.com>
2025-05-09 14:17:12 +0800

    refactor(useEjsMessage): 移除path模块并简化模板路径
    移除对path模块的依赖，直接使用相对路径来加载EJS模板文件，以简化代码并减少不必要的依赖

--- src/utils/useEjsMessage.ts
+++ src/utils/useEjsMessage.ts
@@ -1,5 +1,4 @@
 import ejs from 'ejs';
-import path from 'path';
 
 export const useSummary = () => {
   return async (data: {
@@ -12,9 +11,7 @@ export const useSummary = () => {
     exchangeRate: number;
     unit?: string;
   }) => {
-    const templatePath = path.join(__dirname, '../templates/summary.ejs');
-
-    return await ejs.renderFile(templatePath, data);
+    return await ejs.renderFile('../templates/summary.ejs', data);
   };
 };
 
@@ -27,8 +24,7 @@ export const useDeposit = () => {
     exchangeRate: number;
     unit?: string;
   }) => {
-    const templatePath = path.join(__dirname, '../templates/deposit.ejs');
-    return await ejs.renderFile(templatePath, data);
+    return await ejs.renderFile('../templates/deposit.ejs', data);
   };
 };
 
@@ -41,7 +37,6 @@ export const useWithdraw = () => {
     exchangeRate: number;
     unit?: string;
   }) => {
-    const templatePath = path.join(__dirname, '../templates/withdraw.ejs');
-    return await ejs.renderFile(templatePath, data);
+    return await ejs.renderFile('../templates/withdraw.ejs', data);
   };
 };
