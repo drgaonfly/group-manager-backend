5e2fdf9ac4b71dbe94635ff73d83241fa0e1559c
John Doe <john@example.com>
2025-05-22 20:18:08 +0800

    fix(订阅回调): 更新支付信息中的地址描述和客服链接
    将支付信息中的“trx-20 收款地址”改为“trx-20 转账地址”，并移除客服链接中的具体用户名，改为通用描述

--- src/bot/commands/user/subscription/callback.ts
+++ src/bot/commands/user/subscription/callback.ts
@@ -43,6 +43,8 @@ callbackComposer.callbackQuery(
     });
     const address = bot.trx20_address;
 
+    const customer_service_link = bot.customer_service_link;
+
     if (!address) {
       await ctx.reply('机器人还未设置收款地址');
       return;
@@ -74,11 +76,11 @@ callbackComposer.callbackQuery(
       `<b>订单支付信息</b>\n\n` +
         `订阅类型: ${renewalOption?.label}\n` +
         `订单金额: ${payment.amount} USDT\n\n` +
-        `trx-20 收款地址：\n<code>${address}</code> (点击地址可自动复制)\n\n` +
+        `trx-20 转账地址：\n<code>${address}</code> (点击地址可自动复制)\n\n` +
         `注意事项：\n` +
         `1. 请务必按指定金额转账，否则无法自动化延期。\n` +
         `2. 转账成功10秒钟左右即可自动续费成功。\n` +
-        `3. 如遇到问题，请联系 记账机器人售后客服: @xxx\n\n` +
+        `3. 如遇到问题，请联系 记账机器人售后客服\n\n` +
         `⚠️ 订单将在15分钟后失效，请尽快完成支付`,
       {
         parse_mode: 'HTML',
