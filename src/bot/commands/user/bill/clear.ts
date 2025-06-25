8a6406044bb8872c88b80d4ff6e3360d926a51be
John Doe <john@example.com>
2025-06-25 12:17:32 +0800

    refactor(bot): enhance clear bill command with additional user checks

--- src/bot/commands/user/bill/clear.ts
+++ src/bot/commands/user/bill/clear.ts
@@ -2,29 +2,40 @@ import { Composer } from 'grammy';
 import { MyContext } from '../../../types';
 import createDebug from 'debug';
 import Transaction from '../../../../models/transaction';
+import { isOperatorOrCreator } from '../../../../bot/middlewares/checkBotUser';
+import { checkGroup } from '../../../../bot/middlewares/checkGroup';
+import { checkIsOnline } from '../../../../bot/middlewares/checkIsOnline';
+import { checkPermission } from '../../../middlewares/checkPermission';
 
 const clearBillCommand = new Composer<MyContext>();
 
 const debug = createDebug('bot:bill:clear');
 
 // 清除订单回调处理
-clearBillCommand.hears('清除账单', async (ctx) => {
-  debug('clear_bill callback triggered');
+clearBillCommand.hears(
+  '清除账单',
+  checkGroup,
+  checkPermission,
+  isOperatorOrCreator,
+  checkIsOnline,
+  async (ctx) => {
+    debug('clear_bill callback triggered');
 
-  // 查找今天创建的 exchange
-  const startOfToday = new Date();
-  startOfToday.setHours(0, 0, 0, 0);
-  const endOfToday = new Date();
-  endOfToday.setHours(23, 59, 59, 999);
+    // 查找今天创建的 exchange
+    const startOfToday = new Date();
+    startOfToday.setHours(0, 0, 0, 0);
+    const endOfToday = new Date();
+    endOfToday.setHours(23, 59, 59, 999);
 
-  await Transaction.deleteMany({
-    createdAt: {
-      $gte: startOfToday,
-      $lte: endOfToday,
-    },
-  });
+    await Transaction.deleteMany({
+      createdAt: {
+        $gte: startOfToday,
+        $lte: endOfToday,
+      },
+    });
 
-  ctx.reply(`今日账单已清除`);
-});
+    ctx.reply(`今日账单已清除`);
+  },
+);
 
 export default clearBillCommand;
