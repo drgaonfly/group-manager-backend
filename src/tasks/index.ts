983dd734130f788469a76e01271a1dbd6dc16b7d
dragonfly <2237676323@qq.com>
2025-07-04 13:32:28 +0800

    feat:fix

--- src/tasks/index.ts
+++ src/tasks/index.ts
@@ -9,11 +9,11 @@ import setupDB from '../utils/db';
 // import cron from 'node-cron';
 // import { checkUsdtWallets } from './cron/checkUsdtWallets // 检查usdt转账记录 旧的';
 // import { checkTrxWallets } from './cron/checkTrxWallets' // 检查trx转账记录 旧的;
-// import { checkPendingExchanges } from './cron/checkPendingExchanges';
-// import { checkExpiredExchanges } from './cron/expiredExchange';
-// import { sendGroupMessages } from './cron/groupMessager';
-// import { checkAutoExchanges } from './cron/checkAutoExchanges';
-// import { newCheckTrxWallets } from './cron/getTRXTransactionInfoByAccountAddress';
+import { checkPendingExchanges } from './cron/checkPendingExchanges';
+import { checkExpiredExchanges } from './cron/expiredExchange';
+import { sendGroupMessages } from './cron/groupMessager';
+import { checkAutoExchanges } from './cron/checkAutoExchanges';
+import { newCheckTrxWallets } from './cron/getTRXTransactionInfoByAccountAddress';
 import { newCheckUsdtWallets } from './cron/getUSDTTransactionInfoByAccountAddress';
 import { setupRedis } from '../utils/redis';
 
@@ -31,11 +31,11 @@ const task = async () => {
   // await updateBotExpiration();
   // await checkUsdtWallets(); // 检查usdt转账记录 旧的
   // await checkTrxWallets();// 检查trx转账记录 旧的
-  // await checkExpiredExchanges(); // 检查过期的兑换记录
-  // await checkPendingExchanges(); // 为他人兑换
-  // await checkAutoExchanges(); // 检查授权兑换
-  // await sendGroupMessages(); // 发送群发消息
-  // await newCheckTrxWallets();
+  await checkExpiredExchanges(); // 检查过期的兑换记录
+  await checkPendingExchanges(); // 为他人兑换
+  await checkAutoExchanges(); // 检查授权兑换
+  await sendGroupMessages(); // 发送群发消息
+  await newCheckTrxWallets();
   await newCheckUsdtWallets();
 };
 
