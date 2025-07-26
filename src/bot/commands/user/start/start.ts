52eaabc9a4590fe1eca722eba780bf5b310f4e7d
John Doe <john@example.com>
2025-07-26 20:24:38 +0800

    refactor(startCommand): comment out group message handling logic for clarity and future implementation

--- src/bot/commands/user/start/start.ts
+++ src/bot/commands/user/start/start.ts
@@ -54,20 +54,20 @@ startCommand.command('start', checkPermission, async (ctx) => {
   debug(ctx.chat);
 
   // 判断是否在群组中
-  if (ctx.chat.type !== 'private') {
-    await ctx.reply('感谢您把我添加到贵群!\n下一步设置费率，请发：设置费率x%');
-    debug(`感谢添加！本群 ID 为：${chatId}`);
+  // if (ctx.chat.type !== 'private') {
+  //   await ctx.reply('感谢您把我添加到贵群!\n下一步设置费率，请发：设置费率x%');
+  //   debug(`感谢添加！本群 ID 为：${chatId}`);
 
-    // 检查机器人是否可以读取所有群组消息
-    if (!ctx.me.can_read_all_group_messages) {
-      await ctx.reply(
-        '请关闭机器人隐私模式，否则无法正常工作。\n设置方法：联系 @BotFather，发送 /setprivacy，选择机器人，选择 Disable',
-      );
-      debug('需要关闭机器人隐私模式');
-    }
+  //   // 检查机器人是否可以读取所有群组消息
+  //   if (!ctx.me.can_read_all_group_messages) {
+  //     await ctx.reply(
+  //       '请关闭机器人隐私模式，否则无法正常工作。\n设置方法：联系 @BotFather，发送 /setprivacy，选择机器人，选择 Disable',
+  //     );
+  //     debug('需要关闭机器人隐私模式');
+  //   }
 
-    return;
-  }
+  //   return;
+  // }
 
   await handleStart(ctx);
 
