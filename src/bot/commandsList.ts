7caee149730491c80634439ef333eb2fb589036f
dragonfly <2237676323@qq.com>
2025-07-30 15:03:33 +0800

    feat: fix

--- src/bot/commandsList.ts
+++ src/bot/commandsList.ts
@@ -1,7 +1,6 @@
 // src/commandsList.ts
 export const privateCommandsList = [
   { command: 'start', description: 'Start the bot' },
-  { command: 'profile', description: 'View your profile' },
   { command: 'links', description: 'Top invite rankings' },
   { command: 'my_link', description: 'Your personal invite link' },
 ];
