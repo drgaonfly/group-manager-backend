b0f51628a785a09036c703b8da655e384105cf6c
John Doe <john@example.com>
2025-08-04 22:23:52 +0800

    refactor(commandsList): comment out 'order' command in private and group command lists

--- src/bot/commandsList.ts
+++ src/bot/commandsList.ts
@@ -3,10 +3,10 @@ export const privateCommandsList = [
   { command: 'start', description: 'Start the bot' },
   { command: 'link', description: 'Top invite rankings' },
   // { command: 'my_link', description: 'Your personal invite link' },
-  { command: 'order', description: 'Enter the mini program' },
+  // { command: 'order', description: 'Enter the mini program' },
 ];
 
 export const groupCommandsList = [
   { command: 'link', description: 'Top invite rankings' },
-  { command: 'order', description: 'Enter the mini program' },
+  // { command: 'order', description: 'Enter the mini program' },
 ];
