83428a50c7d7212cfaf0479b181ff15c4b7e0878
John Doe <john@example.com>
2025-11-29 13:26:01 +0800

    refactor(userCommands): comment out unused imports and middleware in user command index

--- src/bot/commands/user/index.ts
+++ src/bot/commands/user/index.ts
@@ -2,20 +2,19 @@
 import { Composer } from 'grammy';
 import startComposer from './start';
 import helpComposer from './help';
-import operatorComposer from './operator';
-import setComposer from './set';
-import billComposer from './bill';
+// import operatorComposer from './operator';
+// import setComposer from './set';
 import contactComposer from './contact';
-import subscriptionComposer from './subscription';
+// import subscriptionComposer from './subscription';
 import profileComposer from './profile';
 import startingComposer from './starting';
-import cloneComposer from './clone';
-import walletComposer from './wallet';
-import exchangeComposer from './exchange';
+// import cloneComposer from './clone';
+// import walletComposer from './wallet';
+// import exchangeComposer from './exchange';
 import conversationsComposer from './conversations';
-import spreadComposer from './spread';
-import linkComposer from './link';
-import orderComposer from './order';
+// import spreadComposer from './spread';
+// import linkComposer from './link';
+// import orderComposer from './order';
 
 // 创建一个新的 Composer 实例
 const userComposer = new Composer();
@@ -24,21 +23,20 @@ userComposer.use(startComposer.middleware());
 userComposer.use(helpComposer.middleware());
 
 // 在群里使用的
-userComposer.use(operatorComposer.middleware());
-userComposer.use(setComposer.middleware());
-userComposer.use(billComposer.middleware());
+// userComposer.use(operatorComposer.middleware());
+// userComposer.use(setComposer.middleware());
 
 // 在机器人使用的
 userComposer.use(profileComposer.middleware());
 userComposer.use(startingComposer.middleware());
 userComposer.use(contactComposer.middleware());
-userComposer.use(subscriptionComposer.middleware());
-userComposer.use(cloneComposer.middleware());
-userComposer.use(walletComposer.middleware());
-userComposer.use(exchangeComposer.middleware());
+// userComposer.use(subscriptionComposer.middleware());
+// userComposer.use(cloneComposer.middleware());
+// userComposer.use(walletComposer.middleware());
+// userComposer.use(exchangeComposer.middleware());
 userComposer.use(conversationsComposer.middleware());
-userComposer.use(spreadComposer.middleware());
-userComposer.use(linkComposer.middleware());
-userComposer.use(orderComposer.middleware());
+// userComposer.use(spreadComposer.middleware());
+// userComposer.use(linkComposer.middleware());
+// userComposer.use(orderComposer.middleware());
 
 export default userComposer;
