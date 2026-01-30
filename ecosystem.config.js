248a3fab8ca77b92de5b33ae7a161975487525da
John Doe <john@example.com>
2026-01-30 12:42:01 +0800

    feat(ecosystem): update cron_restart configuration to execute every 5 minutes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -39,8 +39,8 @@ module.exports = {
         NODE_ENV: 'production',
         DEBUG: 'bot*',
       },
-      // 设置为每 30 分钟执行一次
-      cron_restart: "*/30 * * * *",
+      // 设置为每 5 分钟执行一次
+      cron_restart: "*/5 * * * *",
       // 日志配置
       error_file: './logs/bot-error.log',
       out_file: './logs/bot-out.log',
