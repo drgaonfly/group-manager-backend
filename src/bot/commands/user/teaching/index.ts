9e4ac351eeaeac776857f3c36f5c285d520bb703
dragonfly <2237676323@qq.com>
2026-03-31 15:18:28 +0800

    feat: add setAvailable callback

--- src/bot/commands/user/teaching/index.ts
+++ src/bot/commands/user/teaching/index.ts
@@ -1,11 +1,13 @@
 import { Composer } from 'grammy';
 import findTeacherCommand from './findTeacher';
 import myReviewedsCommand from './myRevieweds';
+import setAvailableCommand from './setAvailable';
 import { MyContext } from '../../../types';
 
 const teachingComposer = new Composer<MyContext>();
 
 teachingComposer.use(findTeacherCommand);
 teachingComposer.use(myReviewedsCommand);
+teachingComposer.use(setAvailableCommand);
 
 export default teachingComposer;
