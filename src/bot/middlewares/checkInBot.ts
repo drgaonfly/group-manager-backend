a8498532fc049eecbccbcccb84361b5f7e7ebf9c
dragonfly <2237676323@qq.com>
2026-04-20 10:54:09 +0800

    feat: remove Please use this command in a private chat with the bot

--- src/bot/middlewares/checkInBot.ts
+++ src/bot/middlewares/checkInBot.ts
@@ -8,7 +8,7 @@ export const checkInBot = async (ctx: MyContext, next: () => Promise<void>) => {
   debug(ctx.chat);
   if (!ctx.chat || ctx.chat.type !== 'private') {
     debug('请在机器人私聊中使用此命令');
-    ctx.reply('Please use this command in a private chat with the bot');
+    // ctx.reply('Please use this command in a private chat with the bot');
     return;
   }
 
