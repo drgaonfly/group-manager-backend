780137ffcc75f5b6d317ceab5035ebfb178e3537
dragonfly <2237676323@qq.com>
2026-05-13 13:47:06 +0800

    feat: move lottery create position

--- src/bot/commands/user/lottery/index.ts
+++ src/bot/commands/user/lottery/index.ts
@@ -6,8 +6,8 @@ import { lotteryHistoryCommand } from './history';
 // 创建一个新的 Composer 实例
 const lotteryComposer = new Composer();
 
-lotteryComposer.use(lotteryCommand.middleware());
 lotteryComposer.use(lotteryCreateCommand.middleware());
 lotteryComposer.use(lotteryHistoryCommand.middleware());
+lotteryComposer.use(lotteryCommand.middleware());
 
 export default lotteryComposer;
