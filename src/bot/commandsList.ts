3c30926948961b78121a02120d6bc0b77faf5169
drgaofly <2237676323@qq.com>
2026-06-27 21:12:15 +0800

    feat: fix bug

--- src/bot/commandsList.ts
+++ src/bot/commandsList.ts
@@ -1,8 +1,6 @@
 // src/commandsList.ts
 export const privateCommandsList = [
   { command: 'start', description: '开始使用' },
-  { command: 'newlottery', description: '创建抽奖' },
-  { command: 'mylottery', description: '我的抽奖' },
 ];
 
 export const groupCommandsList = [
