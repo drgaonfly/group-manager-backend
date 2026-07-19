8040c90eb2e4a929184c08ab8a684684b0dcaf0d
drgaofly <2237676323@qq.com>
2026-07-20 00:15:22 +0800

    feat: remove unused

--- src/bot/commands/user/index.ts
+++ src/bot/commands/user/index.ts
@@ -20,7 +20,6 @@ import veryfiyComposer from './verify';
 import checkinComposer from './checkin';
 import lotteryComposer from './lottery';
 import auctionComposer from './auction';
-import operatorComposer from './operator';
 import conversationsComposer from './conversations';
 
 // import spreadComposer from './spread';
@@ -32,7 +31,6 @@ const userComposer = new Composer();
 
 // conversations 必须最先注册，确保 createConversation 在任何 enter() 调用前生效
 userComposer.use(conversationsComposer.middleware());
-userComposer.use(operatorComposer.middleware());
 userComposer.use(startComposer.middleware());
 userComposer.use(redpacketComposer.middleware());
 userComposer.use(rechargeComposer.middleware());
