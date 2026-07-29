31ef9751fd5cf147d0ef92be8550648c2a9d7ebc
drgaofly <2237676323@qq.com>
2026-07-29 21:56:48 +0800

    feat: remove unused

--- src/bot/middlewares/proxyResolver.ts
+++ src/bot/middlewares/proxyResolver.ts
@@ -8,23 +8,22 @@ const debug = createDebug('botProxy:Resolver');
 const proxyResolver: Middleware<MyContext> = async (ctx, next) => {
   const currentBot = ctx.currentBot;
 
-  const { proxyUser, proxyBotUser, proxyBotUserConfig } =
-    await findBotProxy(currentBot);
+  const { proxyUser } = await findBotProxy(currentBot);
 
   ctx.currentProxyUser = proxyUser;
   if (!proxyUser) {
     debug('找不到代理');
   }
 
-  ctx.currentProxyBotUser = proxyBotUser;
-  if (!proxyBotUser) {
-    debug('找不到代理机器人');
-  }
+  // ctx.currentProxyBotUser = proxyBotUser;
+  // if (!proxyBotUser) {
+  //   debug('找不到代理机器人');
+  // }
 
-  ctx.currentProxyBotUserConfig = proxyBotUserConfig;
-  if (!proxyBotUserConfig) {
-    debug('找不到代理机器人配置');
-  }
+  // ctx.currentProxyBotUserConfig = proxyBotUserConfig;
+  // if (!proxyBotUserConfig) {
+  //   debug('找不到代理机器人配置');
+  // }
 
   // 继续处理后续中间件
   await next();
