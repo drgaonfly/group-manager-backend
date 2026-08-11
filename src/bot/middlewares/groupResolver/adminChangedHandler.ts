68d82d72fc1370b2c2b26e7008d6d97c3af2a6ca
drgaofly <2237676323@qq.com>
2026-08-11 21:38:20 +0800

    feat: remove initial groups

--- src/bot/middlewares/groupResolver/adminChangedHandler.ts
+++ src/bot/middlewares/groupResolver/adminChangedHandler.ts
@@ -96,7 +96,6 @@ export const adminManagementHandler: Middleware<MyContext> = async (
           lastName: memberUser.last_name || '',
           bot: ctx.currentBot._id,
           proxy: proxyUser._id,
-          groups: [], // 初始化空群组数组
         },
         $addToSet: { groups: ctx.currentGroup._id },
       },
